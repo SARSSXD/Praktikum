00e61ff94688b363e90329dbc140bd021404d751
SARSSXD <114547935+SARSSXD@users.noreply.github.com>
2022-12-13 11:21:00 +0700

    Update ProgramStar.java

--- Persiapan UAS/ProgramStar.java
+++ Persiapan UAS/ProgramStar.java
@@ -11,8 +11,8 @@ public class ProgramStar {
         int n = 5;//Integer.parseInt(JOptionPane.showInputDialog("Masukkan Angka Yang Anda Inginkan : "));
 
         //punya Lana
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <= 5; j++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
                 if (i == 1 || i == 5 || j == 1 || j == 5 || i == 3 && j == 3) {
                     System.out.print("* ");
                 } else {
