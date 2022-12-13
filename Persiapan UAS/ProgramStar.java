cb25af08b2f8baeb316cec136aa7593cbe281d40
SARSSXD <114547935+SARSSXD@users.noreply.github.com>
2022-12-13 12:19:28 +0700

    Update ProgramStar.java

--- Persiapan UAS/ProgramStar.java
+++ Persiapan UAS/ProgramStar.java
@@ -108,5 +108,18 @@ public class ProgramStar {
             System.out.print("*");
             System.out.println("");
         }
+        
+        System.out.println("");
+        //huruf D
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if ((i >= 1 && j == 1) || ((i == 1 || i == 5) && j == 2) || ((i == 1 || i == 5) && j == 3) || ((i == 1 || i == 5) && j == 4) || ((i >= 2 && i <= 4) && j == 5) || ((i == 2 || i == 4) && j == 4)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("");
+        }
     }
 }
